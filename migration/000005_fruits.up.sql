a2976d653ac4a4a0465441a8ba4d3c59f80a3b22
Simant Thapa <simantthapa@Simants-MacBook-Air.local>
2022-06-17 16:31:35 +0545

    learning migration cli

---
+++ migration/000005_fruits.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS fruits (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    season VARCHAR(20) NOT NULL,
+    metric VARCHAR(20) NOT NULL,
+    price INT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NULL,
+    deleted_at DATETIME NULL,
+    PRIMARY KEY(id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
