fbcf87a9b2305dc9d375178836cfae9830c9041b
ashish <dhakalashish18@gmail.com>
2022-11-24 14:05:04 +0545

    refactor

--- models/users.go
+++ models/users.go
@@ -1,15 +1,7 @@
 package models
 
-import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/google/uuid"
-	"gorm.io/gorm"
-)
-
 type User struct {
-	BinaryBase
+	Base
 	FirebaseUID string `json:"firebase_uid"`
 	Username    string `json:"username" validate:"required"`
 	Email       string `json:"email" validate:"required"`
@@ -26,23 +18,24 @@ func (m *User) TableName() string {
 // ToMap convert User to map
 func (m User) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"email":     m.Email,
-		"username":  m.Username,
-		"phone":     m.Phone,
-		"full_name": m.FullName,
-		"address":   m.Address,
+		"email":        m.Email,
+		"username":     m.Username,
+		"phone":        m.Phone,
+		"full_name":    m.FullName,
+		"address":      m.Address,
+		"firebase_uid": m.FirebaseUID,
 	}
 }
 
-// Runs before inserting a row into table
-func (m *User) BeforeCreate(db *gorm.DB) error {
-	id, err := uuid.NewRandom()
+// // Runs before inserting a row into table
+// func (m *User) BeforeCreate(db *gorm.DB) error {
+// 	id, err := uuid.NewRandom()
 
-	m.ID = BINARY16(id)
-	fmt.Println(m.ID, "--id----")
-	b, err := json.MarshalIndent(m, "", "")
-	if err == nil {
-		fmt.Println(string(b))
-	}
-	return err
-}
+// 	m.ID = BINARY16(id)
+// 	fmt.Println(m.ID, "--id----")
+// 	b, err := json.MarshalIndent(m, "", "")
+// 	if err == nil {
+// 		fmt.Println(string(b))
+// 	}
+// 	return err
+// }
