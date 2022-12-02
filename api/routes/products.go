344f6f16799f48ff97e8bb6ed97ddcb491cf2419
neekesh <nikesthapa20@gmail.com>
2022-12-02 20:08:35 +0545

    new routes

--- api/routes/products.go
+++ api/routes/products.go
@@ -2,32 +2,43 @@ package routes
 
 import (
 	"boilerplate-api/api/controllers"
+	"boilerplate-api/api/middlewares"
 	"boilerplate-api/infrastructure"
 )
 
+// UserRoutes -> struct
 type ProductRoutes struct {
 	logger            infrastructure.Logger
-	productController controllers.ProductController
 	router            infrastructure.Router
+	productController controllers.ProductController
+	middleware        middlewares.FirebaseAuthMiddleware
+	trxMiddleware     middlewares.DBTransactionMiddleware
 }
 
-func NewProductRoutes(
-	logger infrastructure.Logger,
-	productController controllers.ProductController,
-	router infrastructure.Router,
-) ProductRoutes {
-	return ProductRoutes{
-		logger:            logger,
-		productController: productController,
-		router:            router,
+// Setup user routes
+func (i ProductRoutes) Setup() {
+	i.logger.Zap.Info(" Setting up user routes")
+	products := i.router.Gin.Group("/products")
+	{
+		products.GET("", i.productController.GetAllProducts)
+		products.POST("", i.productController.AddProducts)
+		products.GET("/filter/user/:id", i.productController.AddProducts)
 	}
 }
 
-func (i ProductRoutes) Setup() {
-	i.logger.Zap.Info("Setting Up the Product Routes")
-	product := i.router.Gin.Group("/products")
-	{
-		product.POST("/add", i.productController.AddProducts)
-		product.GET("", i.productController.GetAllProducts)
+// NewProductRoutes -> creates new user controller
+func NewProductRoutes(
+	logger infrastructure.Logger,
+	router infrastructure.Router,
+	productController controllers.ProductController,
+	middleware middlewares.FirebaseAuthMiddleware,
+	trxMiddleware middlewares.DBTransactionMiddleware,
+) ProductRoutes {
+	return ProductRoutes{
+		router:            router,
+		logger:            logger,
+		productController: productController,
+		middleware:        middleware,
+		trxMiddleware:     trxMiddleware,
 	}
 }
