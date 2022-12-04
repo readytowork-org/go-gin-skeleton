adde759a86f1ec8696e144cf7c3910ee23c8e446
neekesh <nikesthapa20@gmail.com>
2022-12-04 11:42:12 +0545

    added sent user for inventory

--- models/products.go
+++ models/products.go
@@ -3,16 +3,17 @@ package models
 import "time"
 
 type Product struct {
-	ItemId       int64  `json:"id"`
-	ItemName     string `json:"product_name"`
-	ReceivedQty  uint64 `json:"received_qty"`
-	SentQty      uint64 `json:"sent_qty"`
-	RemainingQty uint64 `json:"remaining_qty"`
-	ReceivedBy   int64  `json:"received_by"`
-	ReceivedUser User   `gorm:"foreignKey:ReceivedBy" json:"received_user"`
-	// SentBy       int64     `json:"sent_by"`
-	UpdatedAt time.Time `json:"updated_at"`
-	CreatedAt time.Time `json:"created_at"`
+	ItemId       int64     `json:"id"`
+	ItemName     string    `json:"product_name"`
+	ReceivedQty  uint64    `json:"received_qty"`
+	SentQty      uint64    `json:"sent_qty"`
+	RemainingQty uint64    `json:"remaining_qty"`
+	ReceivedBy   int64     `json:"received_by"`
+	ReceivedUser User      `gorm:"foreignKey:ReceivedBy" `
+	SentBy       int64     `json:"sent_by" gorm:"-" `
+	SentUser     User      `gorm:"foreignKey:SentBy" `
+	UpdatedAt    time.Time `json:"updated_at"`
+	CreatedAt    time.Time `json:"created_at"`
 	// SentUser     User      `gorm:"foreignKey:SentBy;"`
 }
 
