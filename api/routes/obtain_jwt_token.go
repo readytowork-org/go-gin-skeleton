e6830da4e984376dd2556efb8e687d18d9ae063f
Ashish Dhakal <dhakalashish18@gmail.com>
2023-01-30 15:37:03 +0545

    feat: setup route to obtain jwt

---
+++ api/routes/obtain_jwt_token.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"boilerplate-api/api/controllers"
+	"boilerplate-api/infrastructure"
+)
+
+// ObtainJwtTokenRoutes -> struct
+type ObtainJwtTokenRoutes struct {
+	logger        infrastructure.Logger
+	router        infrastructure.Router
+	jwtController controllers.JwtAuthController
+}
+
+// Setup Obtain Jwt Token Routes
+func (i ObtainJwtTokenRoutes) Setup() {
+	i.logger.Zap.Info(" Setting up jwt routes")
+	jwt := i.router.Gin.Group("/obtain-jwt-token")
+	{
+		jwt.POST("", i.jwtController.ObtainJwtToken)
+	}
+}
+
+// NewObtainJwtTokenRoutes -> creates new jwt controller
+func NewObtainJwtTokenRoutes(
+	logger infrastructure.Logger,
+	router infrastructure.Router,
+	jwtController controllers.JwtAuthController,
+
+) ObtainJwtTokenRoutes {
+	return ObtainJwtTokenRoutes{
+		router:        router,
+		logger:        logger,
+		jwtController: jwtController,
+	}
+}
