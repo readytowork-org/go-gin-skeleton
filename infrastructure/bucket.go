d5fed1d7fba1ce0fbe8e15480775bdce25eea2fc
Ashish Dhakal <dhakalashish18@gmail.com>
2023-02-14 11:45:32 +0545

    change: undo comment

--- infrastructure/bucket.go
+++ infrastructure/bucket.go
@@ -19,25 +19,25 @@ func NewBucketStorage(logger Logger, env Env) *storage.Client {
 		logger.Zap.Fatal(err.Error())
 	}
 
-	// bucket := client.Bucket(bucketName)
-	// _, err = bucket.Attrs(ctx)
-	// if err == storage.ErrBucketNotExist {
-	// 	logger.Zap.Fatalf("Provided bucket %v doesn't exists", bucketName)
-	// }
-	// if err != nil {
-	// 	logger.Zap.Fatalf("Cloud bucket error: %v", err.Error())
-	// }
-	// bucketAttrsToUpdate := storage.BucketAttrsToUpdate{
-	// 	CORS: []storage.CORS{
-	// 		{
-	// 			MaxAge:          600,
-	// 			Methods:         []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
-	// 			Origins:         []string{"*"},
-	// 			ResponseHeaders: []string{"Content-Type"},
-	// 		}},
-	// }
-	// if _, err := bucket.Update(ctx, bucketAttrsToUpdate); err != nil {
-	// 	logger.Zap.Fatalf("Cloud bucket update error: %v", err.Error())
-	// }
+	bucket := client.Bucket(bucketName)
+	_, err = bucket.Attrs(ctx)
+	if err == storage.ErrBucketNotExist {
+		logger.Zap.Fatalf("Provided bucket %v doesn't exists", bucketName)
+	}
+	if err != nil {
+		logger.Zap.Fatalf("Cloud bucket error: %v", err.Error())
+	}
+	bucketAttrsToUpdate := storage.BucketAttrsToUpdate{
+		CORS: []storage.CORS{
+			{
+				MaxAge:          600,
+				Methods:         []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+				Origins:         []string{"*"},
+				ResponseHeaders: []string{"Content-Type"},
+			}},
+	}
+	if _, err := bucket.Update(ctx, bucketAttrsToUpdate); err != nil {
+		logger.Zap.Fatalf("Cloud bucket update error: %v", err.Error())
+	}
 	return client
 }
