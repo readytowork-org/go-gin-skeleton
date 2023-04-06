b807fd81e891bc45685173bf0faffbfe4a1ef506
Ashish Dhakal <dhakalashish18@gmail.com>
2023-04-07 00:09:05 +0545

    feat: create table categories

---
+++ migration/000002_category.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(100) NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NULL,
+  deleted_at DATETIME NULL,
+  PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
