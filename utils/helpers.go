aaac528e89315efc024b5274a22fa73d63f5b8ee
nikhil <shrestha.nikhil01@gmail.com>
2023-07-06 15:57:25 +0545

    FEAT: add function to map interface

--- utils/helpers.go
+++ utils/helpers.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"golang.org/x/crypto/bcrypt"
+	"reflect"
 )
 
 func CompareHashAndPlainPassword(HashedPassword, PlainPassword string) bool {
@@ -9,4 +10,28 @@ func CompareHashAndPlainPassword(HashedPassword, PlainPassword string) bool {
 		return false
 	}
 	return true
+}
+
+// ToMap takes in an interface{} and converts it into a map[string]interface{} representation.
+func ToMap(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	reflectValue := reflect.ValueOf(data)
+	reflectType := reflect.TypeOf(data)
+
+	for i := 0; i < reflectValue.NumField(); i++ {
+		fieldValue := reflectValue.Field(i)
+		fieldType := reflectType.Field(i)
+
+		if fieldValue.Kind() == reflect.Struct {
+			nestedFields := ToMap(fieldValue.Interface())
+			for nestedKey, nestedValue := range nestedFields {
+				result[nestedKey] = nestedValue
+			}
+		}
+
+		if fieldValue.Kind() != reflect.Struct {
+			result[fieldType.Name] = fieldValue.Interface()
+		}
+	}
+	return result
 }
