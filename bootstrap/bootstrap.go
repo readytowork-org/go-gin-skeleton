2cee657254165290d14f8df0c314e909a675e1f6
Ashish <dhakalashish18@gmail.com>
2023-10-08 00:54:33 +0545

    change: bootstrap

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -1,16 +1,9 @@
 package bootstrap
 
 import (
-	"boilerplate-api/api/controllers"
-	"boilerplate-api/api/middlewares"
-	"boilerplate-api/api/repository"
-	"boilerplate-api/api/routes"
-	"boilerplate-api/api/services"
-	"boilerplate-api/api/validators"
-	"boilerplate-api/cli"
-	"boilerplate-api/docs"
+	"boilerplate-api/config"
 	"boilerplate-api/infrastructure"
-	"boilerplate-api/seeds"
+	"boilerplate-api/middlewares"
 	"boilerplate-api/utils"
 	"context"
 
@@ -19,29 +12,19 @@ import (
 
 // Module exported for initializing application
 var Module = fx.Options(
-	controllers.Module,
-	routes.Module,
-	services.Module,
+	config.InstalledApps,
 	middlewares.Module,
-	repository.Module,
-	validators.Module,
-	infrastructure.Module,
-	cli.Module,
-	seeds.Module,
 	fx.Invoke(bootstrap),
 )
 
 func bootstrap(
 	lifecycle fx.Lifecycle,
 	handler infrastructure.Router,
-	routes routes.Routes,
 	env infrastructure.Env,
 	logger infrastructure.Logger,
-	middlewares middlewares.Middlewares,
 	database infrastructure.Database,
-	cliApp cli.Application,
 	migrations infrastructure.Migrations,
-	seeds seeds.Seeds,
+	routes config.Routes,
 ) {
 
 	appStop := func(context.Context) error {
@@ -56,7 +39,6 @@ func bootstrap(
 			OnStart: func(context.Context) error {
 				logger.Zap.Info("Starting boilerplate cli Application")
 				logger.Zap.Info("------ 🤖 Boilerplate 🤖 (CLI) ------")
-				go cliApp.Start()
 				return nil
 			},
 			OnStop: appStop,
@@ -73,19 +55,11 @@ func bootstrap(
 			logger.Zap.Info("------------------------")
 
 			go func() {
-				if env.Environment != "production" && env.HOST != "" {
-					logger.Zap.Info("Setting Swagger Host...")
-					docs.SwaggerInfo.Host = env.HOST
-				}
-
 				if env.Environment == "production" {
 					logger.Zap.Info("Migrating DB schema...")
 					migrations.Migrate()
 				}
-				middlewares.Setup()
 				routes.Setup()
-				logger.Zap.Info("🌱 seeding data...")
-				seeds.Run()
 				if env.ServerPort == "" {
 					_ = handler.Gin.Run()
 				} else {
