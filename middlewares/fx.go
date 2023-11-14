95199309726434777d8cef9764e971b62b26e7d3
Ashish <dhakalashish18@gmail.com>
2023-11-14 21:35:36 +0545

    chore: dependency injection

--- middlewares/fx.go
+++ middlewares/fx.go
@@ -2,6 +2,7 @@ package middlewares
 
 import "go.uber.org/fx"
 
-var Module = fx.Provide(
-	AuthMiddlewareConstrctor,
+var Module = fx.Options(
+	fx.Provide(NewDBTransaction),
+	fx.Provide(NewJWTAuthMiddleWare),
 )
