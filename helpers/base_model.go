46d4ed42d906fb829ca8720ac129deaaf801d0dd
Ashish <dhakalashish18@gmail.com>
2023-11-14 21:34:45 +0545

    feat: base model

--- helpers/base_model.go
+++ helpers/base_model.go
@@ -8,7 +8,7 @@ import (
 
 type Base struct {
 	Id        int            `gorm:"column:id" json:"id"`
-	CreatedAt int            `gorm:"autoCreateTime;column:created_at" json:"created_at"`
+	CreatedAt time.Time      `gorm:"autoCreateTime;column:created_at" json:"created_at"`
 	UpdatedAt time.Time      `gorm:"autoUpdateTime;column:updated_at" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at" json:"deleted_at"`
 }
