26e93187940c3efce121255d814aa1ad2641edee
Ashish <dhakalashish18@gmail.com>
2023-11-14 22:49:24 +0545

    change: package_name

--- config/conf.go
+++ config/conf.go
@@ -2,9 +2,9 @@ package config
 
 import (
 	"boilerplate-api/apps/auth"
-	authrouter "boilerplate-api/apps/auth/auth_router"
+	"boilerplate-api/apps/auth/auth_router"
 	"boilerplate-api/apps/user"
-	userrouter "boilerplate-api/apps/user/user_router"
+	"boilerplate-api/apps/user/user_router"
 	"boilerplate-api/infrastructure"
 	"boilerplate-api/middlewares"
 
@@ -12,8 +12,8 @@ import (
 )
 
 var InstalledRoutes = fx.Options(
-	fx.Provide(userrouter.RouteConstructor),
-	fx.Provide(authrouter.RouteConstructor),
+	fx.Provide(user_router.RouteConstructor),
+	fx.Provide(auth_router.RouteConstructor),
 )
 
 var InstalledApps = fx.Options(
