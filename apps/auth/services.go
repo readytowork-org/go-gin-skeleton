7e154ccd3990683089f98d13b05f1129c936f559
Ashish <dhakalashish18@gmail.com>
2023-11-14 21:31:02 +0545

    feat: jwt service

---
+++ apps/auth/services.go
@@ -0,0 +1,91 @@
+package auth
+
+import (
+	"boilerplate-api/errors"
+	"boilerplate-api/infrastructure"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Service struct {
+	logger infrastructure.Logger
+	env    infrastructure.Env
+}
+
+func ServiceConstuctor(
+	logger infrastructure.Logger,
+	env infrastructure.Env,
+) Service {
+	return Service{
+		logger: logger,
+		env:    env,
+	}
+}
+
+type JWTClaims struct {
+	jwt.RegisteredClaims
+	// ...other claims
+}
+
+func (m Service) GetTokenFromHeader(c *gin.Context) (string, error) {
+	// Get the token from the request header
+	header := c.GetHeader("Authorization")
+	if header == "" {
+		err := errors.BadRequest.New("Authorization token is required in header")
+		err = errors.SetCustomMessage(err, "Authorization token is required in header")
+		m.logger.Zap.Error("[GetHeader]: ", err.Error())
+		return "", err
+	}
+
+	if !strings.Contains(header, "Bearer") {
+		err := errors.BadRequest.New("Token type is required")
+		m.logger.Zap.Error("Missing token type: ", err.Error())
+		return "", err
+	}
+	tokenString := strings.TrimSpace(strings.Replace(header, "Bearer", "", 1))
+	return tokenString, nil
+
+}
+
+func (m Service) ParseAndVerifyToken(tokenString, secret string) (*jwt.Token, error) {
+	// Parse the token using the secret key
+	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		if !strings.Contains(err.Error(), "expired") {
+			m.logger.Zap.Error("Invalid token[ParseWithClaims] :", err.Error())
+			err := errors.BadRequest.New("Invalid ID token")
+			return nil, err
+		}
+		m.logger.Zap.Error("Invalid token[ParseWithClaims] :", err.Error())
+		return nil, err
+	}
+	return token, nil
+
+}
+
+func (m Service) RetrieveClaims(token *jwt.Token) (*JWTClaims, error) {
+	// Verfify token
+	claims, ok := token.Claims.(*JWTClaims)
+	if !ok || !token.Valid {
+		err := errors.BadRequest.New("Invalid token")
+		err = errors.SetCustomMessage(err, "Invalid token")
+		m.logger.Zap.Error("Invalid token [token.Valid]: ", err.Error())
+		return nil, err
+	}
+	return claims, nil
+
+}
+
+func (m Service) GenerateToken(claims JWTClaims, secret string) (string, error) {
+	// Create a new JWT token using the claims and the secret key
+	tokenClaim := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, tokenErr := tokenClaim.SignedString([]byte(secret))
+	if tokenErr != nil {
+		return "", tokenErr
+	}
+	return token, nil
+}
