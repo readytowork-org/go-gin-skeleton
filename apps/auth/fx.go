bd129107a4c7c3fe282556027e433a559e067316
Ashish <dhakalashish18@gmail.com>
2023-11-14 23:35:39 +0545

    change: remove jwt auth code

--- apps/auth/fx.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import "go.uber.org/fx"
-
-// Module exported for initializing application
-var Module = fx.Options(
-	fx.Provide(ControllerConstuctor),
-	fx.Provide(ServiceConstuctor),
-)
