bd11fd00090c73330e4cd8228c598b7ca4ad4ed0
Ashish <dhakalashish18@gmail.com>
2023-11-14 21:33:35 +0545

    feat: installed routes

--- config/conf.go
+++ config/conf.go
@@ -1,12 +1,26 @@
 package config
 
 import (
+	"boilerplate-api/apps/auth"
+	authrouter "boilerplate-api/apps/auth/auth_router"
+	"boilerplate-api/apps/user"
+	userrouter "boilerplate-api/apps/user/user_router"
 	"boilerplate-api/infrastructure"
+	"boilerplate-api/middlewares"
 
 	"go.uber.org/fx"
 )
 
+var InstalledRoutes = fx.Options(
+	fx.Provide(userrouter.RouteConstructor),
+	fx.Provide(authrouter.RouteConstructor),
+)
+
 var InstalledApps = fx.Options(
+	user.Module,
+	auth.Module,
 	infrastructure.Module,
+	middlewares.Module,
 	RouterModule,
+	InstalledRoutes,
 )
