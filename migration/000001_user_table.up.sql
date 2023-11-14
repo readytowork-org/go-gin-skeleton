edc08ef6227ee7df1000f9d1595dc0f6b8002a30
Ashish <dhakalashish18@gmail.com>
2023-11-14 21:35:48 +0545

    chore: update gender column

--- migration/000001_user_table.up.sql
+++ migration/000001_user_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `full_name` VARCHAR(45) NULL,
   `phone` VARCHAR(15) NULL,
-  `gender` VARCHAR(15) NULL,
+  `gender` VARCHAR(1) NULL,
   `email` VARCHAR(100) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
   `created_at` DATETIME NOT NULL,
