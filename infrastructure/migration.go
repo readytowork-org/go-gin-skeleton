df161fc06b8250733974751e7e3773b46e84aded
Ashish <dhakalashish18@gmail.com>
2024-01-23 16:57:13 +0545

    feat: migration from go code

--- infrastructure/migration.go
+++ infrastructure/migration.go
@@ -1,60 +1,67 @@
 package infrastructure
 
 import (
-	"fmt"
-
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	migrate "github.com/rubenv/sql-migrate"
 )
 
-// Migrations Migration Struct
+// Migrations -> Migration Struct
 type Migrations struct {
 	logger Logger
-	env    Env
+	db     Database
 }
 
-// NewMigrations return new Migrations struct
+// NewMigrations -> return new Migrations struct
 func NewMigrations(
 	logger Logger,
-	env Env,
+	db Database,
 ) Migrations {
 	return Migrations{
 		logger: logger,
-		env:    env,
+		db:     db,
 	}
 }
 
-// Migrate migrates all table
-func (m Migrations) Migrate() {
+// Migrate -> migrates all table
+func (m Migrations) Migrate() error {
 	m.logger.Zap.Info("Migrating schemas...")
 
-	USER := m.env.DBUsername
-	PASS := m.env.DBPassword
-	HOST := m.env.DBHost
-	PORT := m.env.DBPort
-	DBNAME := m.env.DBName
-	ENVIRONMENT := m.env.Environment
+	var plannedMigrations []*migrate.Migration
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
-
-	if ENVIRONMENT != "local" {
-		dsn = fmt.Sprintf(
-			"%s:%s@unix(/cloudsql/%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-			USER,
-			PASS,
-			HOST,
-			DBNAME,
-		)
+	migrationsDir := &migrate.FileMigrationSource{
+		Dir: "migrations/",
 	}
 
-	migrations, err := migrate.New("file://migration/", "mysql://"+dsn)
+	migrationFiles, _ := migrationsDir.FindMigrations()
+	for _, migration := range migrationFiles {
+		// Checks for Empty migration file
+		if len(migration.Up) != 0 {
+			plannedMigrations = append(plannedMigrations, migration)
+		}
+	}
+	memoryMigrations := &migrate.MemoryMigrationSource{
+		Migrations: plannedMigrations,
+	}
+
+	sqlDB, err := m.db.DB.DB()
 	if err != nil {
-		m.logger.Zap.Panic("Error in migration: ", err.Error())
+		return err
 	}
-	m.logger.Zap.Info("--- Running Migration ---")
-	err = migrations.Steps(1000)
+
+	m.logger.Zap.Info("running migration.")
+
+	_, err = migrate.Exec(sqlDB, "mysql", memoryMigrations, migrate.Up)
 	if err != nil {
-		m.logger.Zap.Info("Error in migration steps: ", err.Error())
+		m.logger.Zap.Info(err)
+		return err
 	}
+	m.logger.Zap.Info("migration completed.")
+	return nil
+}
+
+// RunMigration runs the migration provided logger and database instance
+func RunMigration(logger Logger, db Database) error {
+	m := Migrations{logger, db}
+	return m.Migrate()
 }
