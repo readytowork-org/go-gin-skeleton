e1e2ce7a4b068ad19296cb6af6f34f97248d725a
Abinav Ghimire <abighimire07@gmail.com>
2024-03-01 14:59:32 +0545

    :shirt: chore: comments and read me changes

--- redisPubSub/consumer.go
+++ redisPubSub/consumer.go
@@ -23,10 +23,12 @@ func NewMessageConsumer(redis infrastructure.Redis) *MessageConsumer {
 	}
 }
 
+// This Function takes queue names in an array and uses a switch statement to perform required logic for the queues
 func (c *MessageConsumer) ConsumerMessages(ctx context.Context, queueNames []string) {
 	for _, queueName := range queueNames {
 		switch queueName {
 		case "Test":
+			// We will handle the go routines in the custom function
 			go c.handleCustomType1Logic(ctx, queueName)
 		case "Test2":
 			// handleCustomType2Logic(c)
@@ -36,23 +38,32 @@ func (c *MessageConsumer) ConsumerMessages(ctx context.Context, queueNames []str
 	}
 }
 
+// handleCustomType1Logic initiates a goroutine to handle messages from the specified queue.
 func (c *MessageConsumer) handleCustomType1Logic(ctx context.Context, queueName string) {
+
+	// Create a cancellation context to gracefully stop the goroutine
 	consumerCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	log.Printf("[%s] Consumer started listening...\n", queueName)
+
+	// Subscribe to the specified Redis channel
 	c.subscription = c.redisClient.RedisClient.Subscribe(queueName)
 	defer c.subscription.Close()
 
+	// Obtain the channel for receiving messages
 	channel := c.subscription.Channel()
 
 	for {
 		select {
+		// Check if the main context is canceled to stop the goroutine
 		case <-consumerCtx.Done():
 			log.Printf("[%s] Consumer stopped listening...\n", queueName)
 			return
+			// Listen for incoming messages on the channel
 		case msg := <-channel:
 			var messageObj interface{}
+			// Deserialize the message payload
 			err := json.Unmarshal([]byte(msg.Payload), &messageObj)
 			if err != nil {
 				log.Printf("[%s] Failed to deserialize message: %v", queueName, err)
