615e59dfd4682c7c25d407fa629d1f8463aa5877
Sawal Timsina <t.sawal231@gmail.com>
2026-05-08 08:47:33 +0545

    refactor: relocate and update GCP service account key logic to `services` package for enhanced configuration handling

---
+++ services/service_account_key.go
@@ -0,0 +1,19 @@
+package services
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"boilerplate-api/lib/config"
+
+	"google.golang.org/api/option"
+)
+
+func NewGCPClientOption(logger config.Logger, env config.Env) option.ClientOption {
+	serviceAccountKeyFilePath, err := filepath.Abs(fmt.Sprintf("./%v", env.ServiceAccountKey))
+	if err != nil {
+		logger.Panic("Unable to load serviceAccountKey.json file")
+	}
+
+	return option.WithCredentialsFile(serviceAccountKeyFilePath)
+}
