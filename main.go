e7aadf685ae9c93e1802057763ac6c354ced2f2c
Sawal Timsina <t.sawal231@gmail.com>
2026-05-08 08:48:49 +0545

    refactor: remove `godotenv` and integrate Swagger for API documentation

--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"boilerplate-api/bootstrap"
-	"github.com/joho/godotenv"
+	_ "boilerplate-api/swagger"
+
 	"go.uber.org/fx"
 )
 
 //	@title						Demo API
 //	@version					1.0
 //	@description				An API in Go using Gin framework
-//	@host						localhost:8000
 //	@securityDefinitions.apikey	Bearer
 //	@in							header
 //	@name						Authorization
 //	@description				Description for what is this security definition being used
 func main() {
-	_ = godotenv.Load()
 	fx.New(bootstrap.Module).Run()
 }
