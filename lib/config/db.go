745c4db1f20f4061c6f555bd54560ee6fe9acd2e
Sawal Timsina <t.sawal231@gmail.com>
2026-05-22 11:54:33 +0545

    feat: tune database connection pool using environment variables with safe defaults

--- lib/config/db.go
+++ lib/config/db.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -21,7 +22,7 @@ type Database struct {
 }
 
 // NewDatabase creates a new database instance
-func NewDatabase(logger Logger, dbDialect DBDialect) Database {
+func NewDatabase(logger Logger, dbDialect DBDialect, env Env) Database {
 	database := Database{}
 	db, err := gorm.Open(dbDialect, &gorm.Config{Logger: logger.GetGormLogger()})
 	if err != nil {
@@ -44,11 +45,44 @@ func NewDatabase(logger Logger, dbDialect DBDialect) Database {
 			database.connectionError = err
 			return database
 		}
+
+		if sqlDB, sqlErr := db.DB(); sqlErr == nil {
+			applyPoolSettings(sqlDB, env, logger)
+		} else {
+			logger.Warn("could not access underlying *sql.DB to tune pool: ", sqlErr.Error())
+		}
 	}
 
 	logger.Infof("Database connection established : %s", db.Migrator().CurrentDatabase())
 
 	return database
+}
+
+// applyPoolSettings tunes the connection pool from Env, with safe defaults.
+// GORM's default pool is effectively unbounded, which behind Cloud SQL proxy
+// or a small DB can exhaust connections quickly under load.
+func applyPoolSettings(sqlDB interface {
+	SetMaxOpenConns(int)
+	SetMaxIdleConns(int)
+	SetConnMaxLifetime(time.Duration)
+}, env Env, logger Logger) {
+	maxOpen := env.DBMaxOpenConns
+	if maxOpen <= 0 {
+		maxOpen = 25
+	}
+	maxIdle := env.DBMaxIdleConns
+	if maxIdle <= 0 {
+		maxIdle = 5
+	}
+	lifetime := env.DBConnMaxLifetime
+	if lifetime <= 0 {
+		lifetime = 30 * time.Minute
+	}
+
+	sqlDB.SetMaxOpenConns(maxOpen)
+	sqlDB.SetMaxIdleConns(maxIdle)
+	sqlDB.SetConnMaxLifetime(lifetime)
+	logger.Infof("DB pool tuned: maxOpen=%d maxIdle=%d connMaxLifetime=%s", maxOpen, maxIdle, lifetime)
 }
 
 func (d *Database) ConnectionError() error {
